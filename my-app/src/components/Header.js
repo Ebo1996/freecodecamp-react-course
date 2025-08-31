b1b7c0e449a729f3539d5e7c4de5f0203c7b8103
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-08-31 15:06:14 +0300

    update

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <img src="https://m.media-amazon.com/images/I/81PlWn2jZGL._AC_SL1500_.jpg" alt="" />
+    </div>
+  );
+}
+
+export default Header;
