682b897ebdf73e7abbf6851467d711ad6fdc040f
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-09-03 10:00:19 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../src/css/styles.css'
 import Header from './components/Header/Header'
 import First from './components/First/First'
 import Footer from './components/Footer/Footer'
-function App() {
-  return (
-    <div>
-      <Header />
-      <First />
-      <Footer />
-
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <First />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
